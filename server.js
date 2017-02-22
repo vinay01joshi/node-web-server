2d9ff081885d123d1995d1f78b5d21ba6115536a
Vinay Joshi <vinay01joshi@gmail.com>
2017-02-22 20:12:53 +0530

    Add project page

--- server.js
+++ server.js
@@ -39,9 +39,15 @@ app.get('/',(req, res)=>{
 
 app.get('/about',(req,res)=>{
     res.render('about.hbs',{
-        pageTitle : 'About Page Dynamic'
+        pageTitle : 'About'
     });
 });
+
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle : 'Projects'
+    });
+})
 
 app.get('/bad',(req,res)=>{
     res.send({
